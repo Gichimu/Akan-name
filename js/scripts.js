5695171ab4ad76df955e417da79393acb1d340df
Gichimu <gichimueric12@gmail.com>
2019-09-23 11:57:08 +0300

    add changes to js

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,8 @@ var auth = function (){
 
     var DD = parseInt(arr[2]);
 
+    var akan_name = "";
+
     for(i = 0; i < arr.length; i++){
         parseInt(arr[i]);
     }
@@ -29,7 +31,80 @@ var auth = function (){
         return false;
     }else{
         var dotw = (((CC/4) -2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD ) % 7;
-        alert(Math.floor(dotw));
+        //alert(Math.floor(dotw));
+
+        switch (Math.floor(dotw)){
+
+        case 0 || -0:
+            if(gender === "male"){
+                akan_name = "kwasi";
+                alert(akan_name);
+            }else{
+                akan_name = "akosua";
+                alert(akan_name);
+            }
+            break;
+        case 1 || -1:
+        
+            if(gender === "male"){
+                akan_name = "kwadwo";
+                alert(akan_name);
+            }else{
+                akan_name = "adwoa";
+                alert(akan_name);
+            }
+            break;
+        case 2 || -2:
+        
+            if(gender === "male"){
+                akan_name = "kwabena";
+                alert(akan_name);
+            }else{
+                akan_name = "abenaa";
+                alert(akan_name);
+            }
+            break;
+        case 3 || -3:
+    
+            if(gender === "male"){
+                akan_name = "kwaku";
+                alert(akan_name);
+            }else{
+                akan_name = "akua";
+                alert(akan_name);
+            }
+            break;
+        case 4 || -4:
+        
+            if(gender === "male"){
+                akan_name = "Yaw";
+                alert(akan_name);
+            }else{
+                akan_name = "Yaa";
+                alert(akan_name);
+            }
+            break;
+        case 5 || -5:
+            if(gender === "male"){
+                akan_name = "kofi";
+                alert(akan_name);
+            }else{
+                akan_name = "afua";
+                alert(akan_name);
+            }
+            break;
+        case 6 || -6:
+        
+            if(gender === "male"){
+                akan_name = "kwame";
+                alert(akan_name);
+            }else{
+                akan_name = "ama";
+                alert(akan_name);
+            }
+        
+        }
+
     }
     
 }
